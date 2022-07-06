6c7eafde8e31c49d570be73fefd7168f90b8af3f
quxinbo <quxinbo@baidu.com>
2022-07-06 10:37:48 +0800

    feat: init project

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @description: 入口文件
+ * @author: Symbol (quxinbo@baidu.com)
+ * @createTime: 2022-07-06 11:52:25
+ * @lastEditTime: 2022-07-06 11:57:14
+ * @LastEditors: Symbol (quxinbo@baidu.com)
+ */
+
+export const test = () => 100;
+
+export const test2 = () => 200;
